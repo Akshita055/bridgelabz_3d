66e706187de8ec845cebefc94da768991238cdf0
Akshita Singh <168164853+Akshita055@users.noreply.github.com>
2025-09-01 11:51:29 +0530

    Delete practice/oops/encapsulation directory

--- practice/oops/encapsulation/StudentMain.java
+++
@@ -1,9 +0,0 @@
-package encapsulation;
-
-public class StudentMain {
-    public static void main(String[] args) {
-        StudentService studentService = new StudentService();
-        studentService.setStudentDetails();
-        studentService.displayStudentDetails();
-    }
-}
